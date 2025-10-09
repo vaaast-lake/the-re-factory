a21fdc098a4f3169d6e9cf01134f42021ffb1ff2
vaaast-lake <kown9512@gmail.com>
2025-10-09 13:42:10 +0900

    ✨feat: ProductFactory 구현 및 각 프로덕트 별 DTO 작성

--- src/ObserverService.ts
+++ src/ObserverService.ts
@@ -19,6 +19,28 @@ enum ProductType {
     Subscription = 'subscription',
 }
 
+interface RegularProductDTO {
+    productType: ProductType.Regular;
+    name: string;
+    stock: number;
+    price: number;
+}
+
+interface LimitedProductDTO extends Omit<RegularProductDTO, 'productType'> {
+    productType: ProductType.Limited;
+    releaseDate: Date;
+}
+interface SubscriptionProductDTO
+    extends Omit<RegularProductDTO, 'productType'> {
+    productType: ProductType.Subscription;
+    subscriptionPeriod: number;
+}
+
+type ProductDTO =
+    | RegularProductDTO
+    | LimitedProductDTO
+    | SubscriptionProductDTO;
+
 abstract class Product {
     constructor(
         public id: number,
@@ -151,6 +173,52 @@ class SubscriptionProduct extends Product {
 
     addStock(quantity: number) {
         this.stock += quantity;
+    }
+}
+
+class ProductFactory {
+    private static nextId = 1;
+
+    static create(payload: ProductDTO) {
+        const { name, productType, stock, price } = payload;
+
+        switch (payload.productType) {
+            case ProductType.Regular:
+                return new RegularProduct(
+                    this.nextId++,
+                    name,
+                    productType,
+                    stock,
+                    price
+                );
+
+            case ProductType.Limited:
+                const { releaseDate } = payload;
+
+                return new LimitedProduct(
+                    this.nextId++,
+                    name,
+                    productType,
+                    stock,
+                    price,
+                    releaseDate
+                );
+
+            case ProductType.Subscription:
+                const { subscriptionPeriod } = payload;
+                return new SubscriptionProduct(
+                    this.nextId++,
+                    name,
+                    productType,
+                    stock,
+                    price,
+                    subscriptionPeriod
+                );
+
+            default:
+                console.log('There is no Product type.');
+                break;
+        }
     }
 }
 
